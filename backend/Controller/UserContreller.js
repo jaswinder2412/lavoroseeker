900957c9d292523a4f5b5fbe6c823ba5e9ec88f9
Jaswinder <khush@gmail.com>
2022-08-21 04:11:15 -0400

    Updated Functionalities

--- backend/Controller/UserContreller.js
+++ backend/Controller/UserContreller.js
@@ -11,29 +11,85 @@ class UserController{
     }
 
     // Create User
-   async createUser(req){
-    try{
-           req.password =   await bcrypt.hash(req.password, 10);
-           // Save Data
-
-           const userCreated = await User.create(req);
-           const token = jwt.sign(
-            { user_id: userCreated._id },
-            this.sign,
-            {
-              expiresIn: "8h",
-            }
-          );
-          // again save
-          userCreated.token = token;
-         return userCreated.token;
-            
-
-    } catch(error){
-           console.log(error)
-          }
+    async createUser(req){
+        try{
+               req.password =   await bcrypt.hash(req.password, 10);
+               // Save Data in user collection
+               const userCreated = await User.create(req);
+               const token = jwt.sign(
+                { user_id: userCreated._id, email: userCreated.email },
+                this.sign,
+                {
+                  expiresIn: "8h",
+                }
+              );
+              // again save
+              userCreated.token = token;
+             return userCreated.token;
+        } catch(error){
+               console.log(error)
+              }
+          
+        } 
       
-    }
+    // Update User
+    async updateUser(req,userDetails){
+        try{
+            if(req.password){
+                console.log("Password Update")
+                req.password =   await bcrypt.hash(req.password, 10);
+            } else {
+                console.log("Password Not Update")
+                req.password = userDetails.password
+            }
+               // Update Data in user collection
+              
+               const userUpdated = await User.updateOne({'_id':userDetails._id},req);
+               if(req.email != userDetails.email){
+                const token = jwt.sign(
+                    { user_id: userUpdated._id, email: userUpdated.email },
+                    this.sign,
+                    {
+                      expiresIn: "8h",
+                    }
+                  );
+                   //again save
+                  userUpdated.token = token;
+               }
+              
+             return userUpdated;
+        } catch(error){
+               console.log(error)
+              }
+          
+        } 
+        
+        // Login User
+        async loginUser(req,userDetails){
+         try{
+
+            const passwordMatch =   await bcrypt.compare(req.password, userDetails.password);
+           
+               if(passwordMatch){
+                const token = jwt.sign(
+                    { user_id: userDetails._id , email : userDetails.email},
+                    this.sign,
+                    {
+                      expiresIn: "8h",
+                    }
+                  );
+                  // again save token
+                  userDetails.token = token;
+                 return userDetails.token;
+               }else {
+                return false;
+               }
+               
+         } catch(error){
+                console.log(error)
+               }
+           
+         }
 
     //Check user existance
     async userExists(req){
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const uController = require('../Controller/UserContreller')
-
+const authCheck = require('../Middleware/auth')
 
 
 router.get('/',(req,res)=>{
@@ -21,6 +21,53 @@ router.post('/createuser', async (req,res)=>{
     }
 
     
+});
+
+//User Login
+router.post('/login', async (req,res)=>{
+    const userExistance = await uController.userExists(req.fields);
+    if(userExistance.length == 0){
+        res.status(401).send({'error':'User Not Exist'});
+    } else {
+        const userCreation = await uController.loginUser(req.fields,userExistance[0]);
+        if(userCreation){
+            res.status(200).send({'result':userCreation});
+        } else {
+            res.status(401).send({'error':"Credentials Not Matched"});
+        }
+    }
+
+    
+});
+
+//User Details
+router.get('/userdetails',authCheck, async (req,res)=>{
+    const request = JSON.parse(req.user);
+    const userExistance = await uController.userExists(request);
+    if(userExistance.length == 0){
+        res.status(401).send({'error':'User Not Exist'});
+    } else {
+            res.status(200).send({'result':userExistance[0]});
+    }    
+});
+
+//Update User
+router.post('/updateuser',authCheck, async (req,res)=>{
+    const request = JSON.parse(req.user);
+    const userExistance = await uController.userExists(request);
+    if(userExistance.length == 0){
+        res.status(401).send({'error':'User Not Exist'});
+    } else { 
+         
+        const userId = JSON.parse(req.user)
+        if(userExistance[0]._id == userId.user_id){
+            const userUpdation = await uController.updateUser(req.fields,userExistance[0]);
+            res.status(200).send({'result':userUpdation});
+        } else{
+            res.status(409).send({'error':'Email taken already.'});
+        }
+        
+    }    
 });
 
 
